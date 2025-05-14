af4cbe7436053f707e5f8587bccdebc4c26b9908
Khalid <42520040+KhalidRupam@users.noreply.github.com>
2025-05-14 18:11:05 +0600

    Fix

--- BookingSundorbonBackend/Program.cs
+++ BookingSundorbonBackend/Program.cs
@@ -29,9 +29,17 @@ builder.Services.AddCors(
             "http://localhost:3000",
             "https://localhost:3000",
             "https://localhost:3000",
-            "http://localhost:3000", "http://202.126.122.82:33", "http://202.126.122.82:33/api"
-            , "http://sundarbancargo.com", "http://sundarbancargo.com/api"
-            , "https://sundarbancargo.com", "https://sundarbancargo.com/api")
+            "http://localhost:3000", 
+            "http://202.126.122.82:33",
+             "http://202.126.122.82:33/",
+            "http://202.126.122.82:33/api",
+             "https://202.126.122.82:33",
+             "https://202.126.122.82:33/",
+            "https://202.126.122.82:33/api"
+            , "http://sundarbancargo.com", 
+            "http://sundarbancargo.com/api"
+            , "https://sundarbancargo.com", 
+            "https://sundarbancargo.com/api")
             .AllowAnyHeader()
             .AllowAnyMethod()
             );
